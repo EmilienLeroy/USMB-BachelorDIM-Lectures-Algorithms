dbdc9d340e7c4cf7c38561cdc1ed04e153fbccad
EmilienLeroy <emilien1005@hotmail.fr>
2017-10-29 14:07:44 +0100

    Session 3 invert colors numpy and opencv

--- assignments/Session1/S3_imgproc_tools.py
+++ assignments/Session1/S3_imgproc_tools.py
@@ -15,8 +15,36 @@ def invert_colors_manual(input_img):
     
     return input_img
 
+"""
 test = invert_colors_manual(img_bgr)
-
 cv2.imshow("BGR image", test)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+"""
+
+def invert_colors_numpy(input_img):
+    (x,y,z)=(input_img.shape[:3])
+    myMat=np.zeros([x,y,3],dtype=int)
+    myMat[::] = 255;    
+    input_img[::1] = myMat[::1] - input_img[::1]
+    return input_img
+"""
+test = invert_colors_numpy(img_bgr)
+cv2.imshow("BGR image", test)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+"""
+
+def invert_colors_opencv(input_img):
+    b,g,r = cv2.split(input_img)
+    input_img = cv2.merge((255-b,255-g,255-r)) 
+    return input_img
+
+"""
+test = invert_colors_opencv(img_bgr)
+cv2.imshow("BGR image", test)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+"""
+
+
